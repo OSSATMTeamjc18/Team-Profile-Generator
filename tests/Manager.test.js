b98fa62b32138ae49f6ed2c18cebdee332ab404b
Brady Taylor <baetay03@gmail.com>
2021-05-23 22:29:25 -0600

    fixing the test to see if it will pass

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -6,14 +6,16 @@ test('Can set office number from the constructor argument', () => {
   expect(e.officeNumber).toBe(testValue);
 });
 
-test("getRole() should return \"Manager\"", () => {
-  const testValue = 'Manager';
-  const e = new Manager("Foo", 1, 'test@test.com', 100);
-  expect(e.getRole()).toBe(testValue);
-});
 
-test('Can get office number from getOffice()', () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, 'test@test.com', testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
-});
+test("getRole() should return \"Manager\"", () => {
+    const testValue = 'Manager';
+    const e = new Manager("Foo", 1, "test@test.com", 100);
+    expect(e.getRole()).toBe(testValue);
+  });
+  
+
+  test('Can get office number via getOffice()', () => {
+    const testValue = 100;
+    const e = new Manager("Foo", 1, "test@test.com", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+  });
